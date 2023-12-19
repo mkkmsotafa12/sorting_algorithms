99ea98bfb24ee56f61293e38cb614bb44c9dcee7
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-12-19 05:21:31 -0800

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: an array
+ * @size: size of array
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, num;
+	int tmp;
+
+	if (!size || !array)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = size - 1, num = i + 1; j < i; j--)
+		{
+			if (array[j] < array[num])
+			{
+				num = j;
+			}
+		}
+		if (array[i] > array[num])
+		{
+			tmp = array[i];
+			array[i] = array[num];
+			array[num] = tmp;
+			print_array(array, size)
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -27,5 +27,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t *a, listint_t *b);
+void selection_sort(int *array, size_t size);
 
 #endif
